8a14d422ae2d1fc5fe73da0701afe18f39be13e7
George Kailas <gekailas@gmail.com>
2020-12-28 13:56:28 -0800

    5th try, solved pesky tuple pairing problem

--- 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
+++ 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
@@ -11,17 +11,64 @@ Note: This lab might be challenging! Make sure to discuss it with your mentor
 or chat about it on our forum.
 
 '''
-
-input = 3,4,88,9,11,22,44,99
+# Working Start
+input = 3,4,88,33,22,1,1000,23444,23,77
 sinput = (sorted(input))
 counter = 0
-list_toutput = []
+list_output = []
+list_temp = []
 for num in sinput:
-    toutput = ()
-    if counter % 2 != 0:
-        toutput += tuple(num)
-        counter += 1
+    if len(sinput) % 2 ==0:
+        if counter % 2 != 0 or counter ==0:
+            list_temp.append(num)
+            if  counter < len(sinput)-1:
+                counter += 1
+            else: 
+                list_output.append(tuple(list_temp))
+        else:
+            list_output.append(tuple(list_temp))
+            list_temp.append(num)
+            del(list_temp[:2])
+            if  counter < len(sinput)-1:
+                counter += 1
+            else: 
+                list_output.append(tuple(list_temp))
     else:
-        counter +=1
-list_output.append(toutput)
-print(list_toutput)
+        if counter % 2 != 0 or counter ==0:
+            list_temp.append(num)
+            if  counter < len(sinput)-1:
+                counter += 1
+            else: 
+                list_output.append(tuple(list_temp))
+        else:
+            list_output.append(tuple(list_temp))
+            list_temp.append(num)
+            del(list_temp[:2])
+            if  counter < len(sinput)-1:
+                counter += 1
+            else: 
+                list_temp.append(0)
+                list_output.append(tuple(list_temp))
+print(list_output)
+
+# Working Start
+# input = 3,4,88,9,11,22,44,99
+# sinput = (sorted(input))
+# counter = 0
+# list_output = []
+# list_temp = []
+# for num in sinput:
+#     if counter % 2 != 0 or counter ==0:
+#         list_temp.append(num)
+#         counter += 1
+#     else:
+#         list_output.append(tuple(list_temp))
+#         list_temp.append(num)
+#         del(list_temp[:2])
+#         counter +=1
+# list_output.append(tuple(list_temp))
+# print(list_output)
+
+# list_output.append(toutput)
+# print(list_toutput)
+# toutput = ()
