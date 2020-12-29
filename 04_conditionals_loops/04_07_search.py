b6bf077da092b70a555104a307c474828bbf775e
George Kailas <gekailas@gmail.com>
2020-12-28 17:24:53 -0800

    6th, added brute force search

--- 04_conditionals_loops/04_07_search.py
+++ 04_conditionals_loops/04_07_search.py
@@ -4,3 +4,11 @@ Receive a number between 0 and 1,000,000,000 from the user.
 Use while loop to find the number - when the number is found exit the loop and print the number to the console.
 
 '''
+num = int(input("give me a number less than 1000000:"))
+counter = 0
+while counter < 1000000:
+    if counter == num:
+        print(num)
+        break
+    else:
+        counter += 1
